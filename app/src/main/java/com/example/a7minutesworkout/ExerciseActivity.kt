a6afe24fda52338d90a538359d41cf1126a5f196
Danish <iamdanishk555@gmail.com>
2022-04-19 16:18:32 +0545

    Added the Images, Model and Constants for the Exercises

--- app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minutesworkout/ExerciseActivity.kt
@@ -17,6 +17,9 @@ class ExerciseActivity : AppCompatActivity() {
     private var exerciseTimer: CountDownTimer? = null
     private var exerciseProgress = 0
 
+    private var exerciseList: ArrayList<ExerciseModel>? = null
+    private var currentExercisePosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -33,6 +36,8 @@ class ExerciseActivity : AppCompatActivity() {
         }
 
         setupRestView()
+
+        exerciseList = Constants.defaultExerciseList()
     }
 
     private fun setRestProgressBar(){
@@ -48,9 +53,10 @@ class ExerciseActivity : AppCompatActivity() {
 
             override fun onFinish() {
 
-                Toast.makeText(this@ExerciseActivity,
-                    "Now, we'll start the exercise!!", Toast.LENGTH_SHORT).show()
-                
+                //Toast.makeText(this@ExerciseActivity, "Now, we'll start the exercise!!", Toast.LENGTH_SHORT).show()
+
+                currentExercisePosition++
+
                 setupExerciseView()
             }
         }.start()
@@ -68,13 +74,27 @@ class ExerciseActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                Toast.makeText(this@ExerciseActivity,
-                    "30 secs are over, lets go to the rest view!!", Toast.LENGTH_SHORT).show()
+                
+                //Toast.makeText(this@ExerciseActivity, "30 secs are over, lets go to the rest view!!", Toast.LENGTH_SHORT).show()
+
+                if (currentExercisePosition <exerciseList?.size!! - 1){
+                    setupRestView()
+                }
+                else{
+                    Toast.makeText(this@ExerciseActivity,
+                        "Congratulations! You have completed the 7 minutes workout.", Toast.LENGTH_SHORT).show()
+                }
             }
         }.start()
     }
 
     private fun setupRestView(){
+        binding?.flRestView?.visibility = View.VISIBLE
+        binding?.tvTitle?.visibility = View.VISIBLE
+        binding?.tvExerciseName?.visibility = View.INVISIBLE
+        binding?.flExerciseView?.visibility = View.INVISIBLE
+        binding?.ivImage?.visibility = View.INVISIBLE
+
         if (restTimer != null){
             restTimer?.cancel()
             restProgress = 0
@@ -83,14 +103,19 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setupExerciseView(){
-        binding?.flProgressBar?.visibility = View.INVISIBLE
-        binding?.tvTitle?.text = "Exercise Name"
+        binding?.flRestView?.visibility = View.INVISIBLE
+        binding?.tvTitle?.visibility = View.INVISIBLE
+        binding?.tvExerciseName?.visibility = View.VISIBLE
         binding?.flExerciseView?.visibility = View.VISIBLE
+        binding?.ivImage?.visibility = View.VISIBLE
 
         if (exerciseTimer != null){
             exerciseTimer?.cancel()
             exerciseProgress = 0
         }
+
+        binding?.ivImage?.setImageResource(exerciseList!![currentExercisePosition].getImage())
+        binding?.tvExerciseName?.text = exerciseList!![currentExercisePosition].getName()
 
         setExerciseProgressBar()
     }
